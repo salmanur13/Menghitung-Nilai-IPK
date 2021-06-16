60c705f70d8269044aafe6a79b867ca3a7f3cf04
salmanur13 <85994212+salmanur13@users.noreply.github.com>
2021-06-16 17:48:14 +0700

    Add files via upload

---
+++ Tubes UTS(IPK) Prokom Sem2/Menghitung Nilai IPK/Form1.Designer.vb
@@ -0,0 +1,375 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TxtMatkul4 = New System.Windows.Forms.TextBox()
+        Me.TxtMatkul3 = New System.Windows.Forms.TextBox()
+        Me.TxtMatkul2 = New System.Windows.Forms.TextBox()
+        Me.TxtMatkul1 = New System.Windows.Forms.TextBox()
+        Me.TxtMatkul5 = New System.Windows.Forms.TextBox()
+        Me.TxtMatkul6 = New System.Windows.Forms.TextBox()
+        Me.Txtsks6 = New System.Windows.Forms.TextBox()
+        Me.Txtsks5 = New System.Windows.Forms.TextBox()
+        Me.Txtsks1 = New System.Windows.Forms.TextBox()
+        Me.Txtsks2 = New System.Windows.Forms.TextBox()
+        Me.Txtsks3 = New System.Windows.Forms.TextBox()
+        Me.Txtsks4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TxtIP = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtCumlaude = New System.Windows.Forms.TextBox()
+        Me.txtSelamat = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 77)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fisdas 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 98)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Kimia"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 122)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Kalkulus"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 146)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(140, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Pengantar Teknik Industri"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 172)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Menggambar Teknik"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 196)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Pancasila"
+        '
+        'TxtMatkul4
+        '
+        Me.TxtMatkul4.Location = New System.Drawing.Point(177, 148)
+        Me.TxtMatkul4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul4.Name = "TxtMatkul4"
+        Me.TxtMatkul4.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul4.TabIndex = 6
+        '
+        'TxtMatkul3
+        '
+        Me.TxtMatkul3.Location = New System.Drawing.Point(177, 124)
+        Me.TxtMatkul3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul3.Name = "TxtMatkul3"
+        Me.TxtMatkul3.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul3.TabIndex = 7
+        '
+        'TxtMatkul2
+        '
+        Me.TxtMatkul2.Location = New System.Drawing.Point(177, 100)
+        Me.TxtMatkul2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul2.Name = "TxtMatkul2"
+        Me.TxtMatkul2.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul2.TabIndex = 8
+        '
+        'TxtMatkul1
+        '
+        Me.TxtMatkul1.Location = New System.Drawing.Point(177, 76)
+        Me.TxtMatkul1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul1.Name = "TxtMatkul1"
+        Me.TxtMatkul1.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul1.TabIndex = 9
+        '
+        'TxtMatkul5
+        '
+        Me.TxtMatkul5.Location = New System.Drawing.Point(177, 174)
+        Me.TxtMatkul5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul5.Name = "TxtMatkul5"
+        Me.TxtMatkul5.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul5.TabIndex = 10
+        '
+        'TxtMatkul6
+        '
+        Me.TxtMatkul6.Location = New System.Drawing.Point(177, 196)
+        Me.TxtMatkul6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtMatkul6.Name = "TxtMatkul6"
+        Me.TxtMatkul6.Size = New System.Drawing.Size(91, 23)
+        Me.TxtMatkul6.TabIndex = 11
+        '
+        'Txtsks6
+        '
+        Me.Txtsks6.Location = New System.Drawing.Point(296, 194)
+        Me.Txtsks6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks6.Name = "Txtsks6"
+        Me.Txtsks6.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks6.TabIndex = 17
+        '
+        'Txtsks5
+        '
+        Me.Txtsks5.Location = New System.Drawing.Point(296, 172)
+        Me.Txtsks5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks5.Name = "Txtsks5"
+        Me.Txtsks5.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks5.TabIndex = 16
+        '
+        'Txtsks1
+        '
+        Me.Txtsks1.Location = New System.Drawing.Point(296, 74)
+        Me.Txtsks1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks1.Name = "Txtsks1"
+        Me.Txtsks1.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks1.TabIndex = 15
+        '
+        'Txtsks2
+        '
+        Me.Txtsks2.Location = New System.Drawing.Point(296, 98)
+        Me.Txtsks2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks2.Name = "Txtsks2"
+        Me.Txtsks2.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks2.TabIndex = 14
+        '
+        'Txtsks3
+        '
+        Me.Txtsks3.Location = New System.Drawing.Point(296, 122)
+        Me.Txtsks3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks3.Name = "Txtsks3"
+        Me.Txtsks3.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks3.TabIndex = 13
+        '
+        'Txtsks4
+        '
+        Me.Txtsks4.Location = New System.Drawing.Point(296, 148)
+        Me.Txtsks4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Txtsks4.Name = "Txtsks4"
+        Me.Txtsks4.Size = New System.Drawing.Size(52, 23)
+        Me.Txtsks4.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(63, 28)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(164, 30)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "NILAI MATKUL"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(164, 256)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 25)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Hitung"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TxtIP
+        '
+        Me.TxtIP.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TxtIP.Location = New System.Drawing.Point(458, 94)
+        Me.TxtIP.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TxtIP.Name = "TxtIP"
+        Me.TxtIP.Size = New System.Drawing.Size(106, 23)
+        Me.TxtIP.TabIndex = 21
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(467, 64)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(74, 15)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "IP SEMESTER"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(289, 16)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(74, 50)
+        Me.Label10.TabIndex = 24
+        Me.Label10.Text = "BOBOT" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "SKS"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(8, 5)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(64, 15)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "Hitung IPK"
+        '
+        'txtCumlaude
+        '
+        Me.txtCumlaude.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtCumlaude.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtCumlaude.Font = New System.Drawing.Font("Prestige Elite Std", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtCumlaude.Location = New System.Drawing.Point(372, 157)
+        Me.txtCumlaude.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtCumlaude.Multiline = True
+        Me.txtCumlaude.Name = "txtCumlaude"
+        Me.txtCumlaude.Size = New System.Drawing.Size(247, 83)
+        Me.txtCumlaude.TabIndex = 26
+        Me.txtCumlaude.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.txtCumlaude.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtSelamat
+        '
+        Me.txtSelamat.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtSelamat.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtSelamat.Font = New System.Drawing.Font("Prestige Elite Std", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtSelamat.Location = New System.Drawing.Point(372, 128)
+        Me.txtSelamat.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtSelamat.Name = "txtSelamat"
+        Me.txtSelamat.Size = New System.Drawing.Size(247, 26)
+        Me.txtSelamat.TabIndex = 27
+        Me.txtSelamat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(310, 256)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 25)
+        Me.Button2.TabIndex = 28
+        Me.Button2.Text = "Lihat Data"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(642, 316)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtSelamat)
+        Me.Controls.Add(Me.txtCumlaude)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TxtIP)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Txtsks6)
+        Me.Controls.Add(Me.Txtsks5)
+        Me.Controls.Add(Me.Txtsks1)
+        Me.Controls.Add(Me.Txtsks2)
+        Me.Controls.Add(Me.Txtsks3)
+        Me.Controls.Add(Me.Txtsks4)
+        Me.Controls.Add(Me.TxtMatkul6)
+        Me.Controls.Add(Me.TxtMatkul5)
+        Me.Controls.Add(Me.TxtMatkul1)
+        Me.Controls.Add(Me.TxtMatkul2)
+        Me.Controls.Add(Me.TxtMatkul3)
+        Me.Controls.Add(Me.TxtMatkul4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "IPK"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TxtMatkul4 As TextBox
+    Friend WithEvents TxtMatkul3 As TextBox
+    Friend WithEvents TxtMatkul2 As TextBox
+    Friend WithEvents TxtMatkul1 As TextBox
+    Friend WithEvents TxtMatkul5 As TextBox
+    Friend WithEvents TxtMatkul6 As TextBox
+    Friend WithEvents Txtsks6 As TextBox
+    Friend WithEvents g As TextBox
+    Friend WithEvents Txtsks1 As TextBox
+    Friend WithEvents Txtsks2 As TextBox
+    Friend WithEvents Txtsks3 As TextBox
+    Friend WithEvents Txtsks4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TxtIP As TextBox
+    Friend WithEvents Txtsks5 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtCumlaude As TextBox
+    Friend WithEvents txtSelamat As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Tubes UTS(IPK) Prokom Sem2/Menghitung Nilai IPK/Form1.vb
@@ -0,0 +1,207 @@
+﻿Public Class Form1
+    Dim matkul1, matkul2, matkul3, matkul4, matkul5, matkul6 As Double
+    Dim sks1, sks2, sks3, sks4, sks5, sks6 As Integer
+    Dim NA1, NA2, NA3, NA4, NA5, NA6 As String
+    Dim NilaiMK As String
+    Dim Text As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        matkul1 = TxtMatkul1.Text
+        If matkul1 >= "80" And matkul1 <= "100" Then
+            NA1 = "4"
+
+        ElseIf matkul1 >= "75" And matkul1 <= "79,99" Then
+            NA1 = "3,5"
+
+        ElseIf matkul1 >= "70" And matkul1 <= "74,99" Then
+            NA1 = "3"
+
+        ElseIf matkul1 >= "65" And matkul1 <= "69,99" Then
+            NA1 = "2,5"
+
+        ElseIf matkul1 >= "60" And matkul1 <= "64,99" Then
+            NA1 = "2"
+
+        ElseIf matkul1 >= "56" And matkul1 <= "59,99" Then
+            NA1 = "1,5"
+
+        ElseIf matkul1 >= "46" And matkul1 <= "55,99" Then
+            NA1 = "1"
+
+        ElseIf matkul1 >= "0" And matkul1 <= "45,99" Then
+            NA1 = "0"
+        End If
+
+        matkul2 = TxtMatkul2.Text
+        If matkul2 >= "80" And matkul2 <= "100" Then
+            NA2 = "4"
+
+        ElseIf matkul2 >= "75" And matkul2 <= "79,99" Then
+            NA2 = "3,5"
+
+        ElseIf matkul2 >= "70" And matkul2 <= "74,99" Then
+            NA2 = "3"
+
+        ElseIf matkul2 >= "65" And matkul2 <= "69,99" Then
+            NA2 = "2,5"
+
+        ElseIf matkul2 >= "60" And matkul2 <= "64,99" Then
+            NA2 = "2"
+
+        ElseIf matkul2 >= "56" And matkul2 <= "59,99" Then
+            NA2 = "1,5"
+
+        ElseIf matkul2 >= "46" And matkul2 <= "55,99" Then
+            NA2 = "1"
+
+        ElseIf matkul2 >= "0" And matkul2 <= "45,99" Then
+            NA2 = "0"
+
+        End If
+
+        matkul3 = TxtMatkul3.Text
+        If matkul3 >= "80" Then
+            NA3 = "4"
+
+        ElseIf matkul3 >= "75" And matkul3 <= "79,99" Then
+            NA3 = "3,5"
+
+        ElseIf matkul3 >= "70" And matkul3 <= "74,99" Then
+            NA3 = "3"
+
+        ElseIf matkul3 >= "65" And matkul3 <= "69,99" Then
+            NA3 = "2,5"
+
+        ElseIf matkul3 >= "60" And matkul3 <= "64,99" Then
+            NA3 = "2"
+
+        ElseIf matkul3 >= "56" And matkul3 <= "59,99" Then
+            NA3 = "1,5"
+
+        ElseIf matkul3 >= "46" And matkul3 <= "55,99" Then
+            NA3 = "1"
+
+        ElseIf matkul3 >= "0" And matkul3 <= "45,99" Then
+            NA3 = "0"
+
+        End If
+
+        matkul4 = TxtMatkul4.Text
+        If matkul4 >= "80" Then
+            NA4 = "4"
+
+        ElseIf matkul4 >= "75" And matkul4 <= "79,99" Then
+            NA4 = "3,5"
+
+        ElseIf matkul4 >= "70" And matkul4 <= "74,99" Then
+            NA4 = "3"
+
+        ElseIf matkul4 >= "65" And matkul4 <= "69,99" Then
+            NA4 = "2,5"
+
+        ElseIf matkul4 >= "60" And matkul4 <= "64,99" Then
+            NA4 = "2"
+
+        ElseIf matkul4 >= "56" And matkul4 <= "59,99" Then
+            NA4 = "1,5"
+
+        ElseIf matkul4 >= "46" And matkul4 <= "55,99" Then
+            NA4 = "1"
+
+        ElseIf matkul4 >= "0" And matkul4 <= "45,99" Then
+            NA4 = "0"
+
+        End If
+
+        matkul5 = TxtMatkul5.Text
+        If matkul5 >= "80" Then
+            NA5 = "4"
+
+        ElseIf matkul5 >= "75" And matkul5 <= "79,99" Then
+            NA5 = "3,5"
+
+        ElseIf matkul5 >= "70" And matkul5 <= "74,99" Then
+            NA5 = "3"
+
+        ElseIf matkul5 >= "65" And matkul5 <= "69,99" Then
+            NA5 = "2,5"
+
+        ElseIf matkul5 >= "60" And matkul5 <= "64,99" Then
+            NA5 = "2"
+
+        ElseIf matkul5 >= "56" And matkul5 <= "59,99" Then
+            NA5 = "1,5"
+
+        ElseIf matkul5 >= "46" And matkul5 <= "55,99" Then
+            NA5 = "1"
+
+        ElseIf matkul5 >= "0" And matkul5 <= "45,99" Then
+            NA5 = "0"
+
+
+        End If
+
+        matkul6 = TxtMatkul6.Text
+        If matkul6 >= "80" Then
+            NA6 = "4"
+
+        ElseIf matkul6 >= "75" And matkul6 <= "79,99" Then
+            NA6 = "3,5"
+
+        ElseIf matkul6 >= "70" And matkul6 <= "74,99" Then
+            NA6 = "3"
+
+        ElseIf matkul6 >= "65" And matkul6 <= "69,99" Then
+            NA6 = "2,5"
+
+        ElseIf matkul6 >= "60" And matkul6 <= "64,99" Then
+            NA6 = "2"
+
+        ElseIf matkul6 >= "56" And matkul6 <= "59,99" Then
+            NA6 = "1,5"
+
+        ElseIf matkul6 >= "46" And matkul6 <= "55,99" Then
+            NA6 = "1"
+
+        ElseIf matkul6 >= "0" And matkul6 <= "45,99" Then
+            NA6 = "0"
+
+        End If
+
+        sks1 = Txtsks1.Text
+        sks2 = Txtsks2.Text
+        sks3 = Txtsks3.Text
+        sks4 = Txtsks4.Text
+        sks5 = Txtsks5.Text
+        sks6 = Txtsks6.Text
+
+        NilaiMK = (NA1 * sks1) + (NA2 * sks2) + (NA3 * sks3) + (NA4 * sks4) + (NA5 * sks5) + (NA6 * sks6)
+        TxtIP.Text = NilaiMK / (sks1 + sks2 + sks3 + sks4 + sks5 + sks6)
+
+        Text = TxtIP.Text
+        If Text >= 3.5 Then
+            txtSelamat.Text = "Yeyy Selamat"
+            txtCumlaude.Text = "Anda Cumlaude"
+        ElseIf TxtIP.Text >= 3 And TxtIP.Text <= 3.49 Then
+            txtSelamat.Text = "Selamat"
+            txtCumlaude.Text = "Anda luar Biasa "
+        ElseIf TxtIP.Text >= 2 And TxtIP.Text <= 2.99 Then
+            txtSelamat.Text = "Semangat!"
+            txtCumlaude.Text = "Masih Ada Waktu Untuk Perbaiki"
+        ElseIf TxtIP.Text >= 1 And TxtIP.Text <= 1.99 Then
+            txtSelamat.Text = "Semangat!"
+            txtCumlaude.Text = "JANGAN PANTANG MENYERAH"
+        ElseIf TxtIP.Text >= 0 And TxtIP.Text <= 0.99 Then
+            txtSelamat.Text = "Semangat!"
+            txtCumlaude.Text = "INI NILAI ANDA SUDAH MENGKHAWATIRKAN MASA DEPAN"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Fisika Dasar 1: " & NA1 & Environment.NewLine & "Kimia: " & NA2 & Environment.NewLine &
+                         "Kalkulus : " & NA3 & Environment.NewLine & "Pengantar Teknik Industri : " & NA4 & Environment.NewLine & "Menggambar Teknik: " & NA5 & Environment.NewLine &
+                         "Pancasila : " & NA6 & Environment.NewLine & Environment.NewLine & "Total IP Semester: " & TxtIP.Text & Environment.NewLine &
+                         Environment.NewLine & Environment.NewLine & txtSelamat.Text & Environment.NewLine & txtCumlaude.Text, "Nilai IP Semester")
+
+
+    End Sub
+End Class
